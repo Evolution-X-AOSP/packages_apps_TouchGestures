b4808d4eb6811330aace693beb7ec3712bae0b07
Joey Huab <joey@evolution-x.org>
2020-01-31 07:02:51 +0000

    TouchGestures: Remove dependencies

--- Android.mk
+++ Android.mk
@@ -6,6 +6,18 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_USE_AAPT2 := true
 
+LOCAL_STATIC_JAVA_LIBRARIES += \
+    androidx.annotation_annotation \
+    androidx.lifecycle_lifecycle-common
+
+LOCAL_STATIC_ANDROID_LIBRARIES += \
+    androidx.legacy_legacy-support-v4 \
+    androidx.lifecycle_lifecycle-runtime \
+    androidx.recyclerview_recyclerview \
+    androidx.preference_preference \
+    androidx.appcompat_appcompat \
+    androidx.legacy_legacy-preference-v14
+
 LOCAL_PACKAGE_NAME := TouchGestures
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
@@ -14,7 +26,6 @@ LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 include frameworks/base/packages/SettingsLib/common.mk
-include vendor/support/common.mk
 
 include $(BUILD_PACKAGE)
 
